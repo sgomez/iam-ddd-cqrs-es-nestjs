e1d4d4bfd1f373e4516908a307b22456ec9fd03a
Sergio Gómez <sergio@uco.es>
2019-10-03 18:17:16 +0200

    environment update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('IAM');
 
   const options = new DocumentBuilder()
     .setTitle('IAM')
@@ -13,6 +16,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0', () => {
+    logger.log('Started at http://localhost:3000/api');
+  });
 }
 bootstrap();
